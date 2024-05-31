562f59d6eafceb40103cb8fde230c62b407f62e2
morghanhartmann <morghan.hartmann@beyondblood.co>
2024-05-31 14:08:17 +0100

    Added Word2Vec

--- recipes.py
+++ recipes.py
@@ -3,8 +3,22 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import re
 from selenium.webdriver.common.action_chains import ActionChains
+from gensim.models import Word2Vec
+import pandas as pd
+
+import re
+
+from sklearn.decomposition import PCA
+
+from matplotlib import pyplot as plt
+import plotly.graph_objects as go
+
+import numpy as np
+
+import warnings
 
 driver = selenium.webdriver.Chrome()
 driver.get("https://www.bbc.co.uk/food/collections/affordable_30-minute_meals_for_four")
@@ -17,14 +31,17 @@ link_list = []
 for i in links:
     link_list.append(i.text)
 link_list = list(filter(None, link_list))
-link_list = link_list[30:35]  #for quick testing use this
+link_list = link_list[30:34]  #for quick testing use this
 #link_list = link_list[30:] #for full list
-#print(link_list) #for testing
 
+
+total_list = []
 for recipe in link_list:
+    wait = WebDriverWait(driver, 5)
     wait.until(EC.element_to_be_clickable((By.LINK_TEXT, recipe))).click()
     time.sleep(1)
     items = driver.find_elements(By.XPATH, "//li[@class='recipe-ingredients__list-item']")
+
     ingredient_list = []
     for item in items:
         text = item.text
@@ -55,13 +72,21 @@ for recipe in link_list:
             text_split_3 = text_split_2
 
         ingredient_list.append(text_split_3)
-        #print(text_split_3,'\n')
-
-    print(ingredient_list)
-
+    #print(ingredient_list)
+    total_list.append(ingredient_list)
+    #print(total_list)
     #driver.navigate().back() #less reliable way for back button
     driver.execute_script("window.history.go(-1)")
     time.sleep(2)
+print(total_list)
+
+# train word2vec model
+model = (Word2Vec(total_list, min_count=1, vector_size = 3))
+print(model)
+
+# find similar words
+sims = model.wv.most_similar('kale', topn=10)
+print(sims)
 
 #close browser
 #driver.quit()
